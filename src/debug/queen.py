1ec8bc76d8cd952cc6ac7b3f693c3d5da140cd38
whompyjaw <whompyjaw@mbp>
2020-12-07 21:15:31 -0800

    got positions of unit correctly

--- src/debug/queen.py
+++ src/debug/queen.py
@@ -1,7 +1,8 @@
-import sys,os
+import sys, os
 from contextlib import suppress
 import sc2
 import logging
+import math
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.ability_id import AbilityId
@@ -11,27 +12,47 @@ from sc2.units import Units
 from sc2.position import Point2, Point3
 
 
-
 class DebugQueen(sc2.BotAI):
-    '''
-        Where we can test small problems within a vacuum
-    '''
+    """
+    Where we can test small problems within a vacuum
+    """
+
     async def on_step(self, iteration):
         if iteration == 0:
             await self.client.debug_show_map()
             # create queen at first hatch
-            await self.client.debug_create_unit([[UnitTypeId.QUEEN, 1, self.start_location, 1]])
+            await self.client.debug_create_unit(
+                [[UnitTypeId.QUEEN, 1, self.start_location, 1]]
+            )
         await self.spread_creep()
+
     async def spread_creep(self):
+        positions = []
         build_tumor = AbilityId.BUILD_CREEPTUMOR_QUEEN
         queens = self.units(UnitTypeId.QUEEN)
         for queen in queens:
+            pos = self.get_pos_around_unit(queen)
             if queen.energy >= 25:
                 queen(build_tumor, 3)
 
 
-run_game(maps.get("AbyssalReefLE"), [Bot(Race.Zerg, DebugQueen()), 
-    Computer(Race.Terran, Difficulty.Easy)], realtime=False)
+    def get_pos_around_unit(self, unit, min_range=0, max_range=500, step_size=1, loc_amt=32):
+        loc = unit.position.to2
+        # loc = unit
+        positions = [Point2(( \
+            loc.x + distance * math.cos(math.pi * 2 * alpha / loc_amt), \
+            loc.y + distance * math.sin(math.pi * 2 * alpha / loc_amt))) \
+            for alpha in range(loc_amt) # alpha is the angle here, locationAmount is the variable on how accurate the attempts look like a circle (= how many points on a circle)
+            for distance in range(min_range, max_range+1)] # distance depending on minrange and maxrange
+        return positions
+        
+        
+
+run_game(
+    maps.get("AbyssalReefLE"),
+    [Bot(Race.Zerg, DebugQueen()), Computer(Race.Terran, Difficulty.Easy)],
+    realtime=False,
+)
 
 
 # Reference
