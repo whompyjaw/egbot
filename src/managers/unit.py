b053f20659309ec5ca578b26d39d500cd42bd5a8
wj <wj>
2021-01-09 12:40:03 -0500

    updated assign_queen

--- src/managers/unit.py
+++ src/managers/unit.py
@@ -21,6 +21,7 @@ class UnitManager:
         self.queen = UnitTypeId.QUEEN
         self.drone_name = "Drone"
         self.queen_name = "Queen"
+        self.queen_home = {}
         self.hatcheries = None
         self.mm = MacroManager(self.bot)
         self.drones = []
@@ -34,7 +35,6 @@ class UnitManager:
     def update_units(self):
         self.larvae = self.bot.larva
 
-
     def add_unit(self, unit: Unit):
         """TODO: got to run, but found error: unit.name = 'Drone' or 'Queen' while self.drone.name = 'QUEEN'
         when we compare the two strings they will not be equal and therefore no drones or queens get appended.
@@ -47,28 +47,25 @@ class UnitManager:
             self.assign_queen(new_queen)
 
     def assign_queen(self, queen: Queen):
-        '''TODO: Everything works EXCEPT queens_no_inject_partner - for some reason my list comprehension doesn't work
-        The first queen through goes to .is_creep perferctly.  Second queen goes through and is matched with the closest
-        hatchery and assigned .is_hatch.  I verified within queen.hatch_home that the queen tag is the key, and the hatch tag
-        is the value.  However, when the third queen pops, queens_no_inject_partner still contains the previous .is_hatch 
-        queen and thus the loop happens and hits the break with no action as that queen is already assigned.  Need to figure out
-        how to remove queens already tagged to a hatch, thus I know my list comprehension is failing.'''
-        queens_no_inject_partner = [q for q.tag in self.queens if q.tag not in queen.hatch_home.keys()]
-        bases_no_inject_partner = self.bot.townhalls.filter(lambda h: h.tag not in queen.hatch_home.values())
+        '''Assigns a queen as a Creep Queen or a Hatch Queen.  If Hatch Queen, assigns the queen to a specific hatchery for future larva injects '''
+        queens_no_inject_partner = [q for q in self.queens if q.tag not in self.queen_home.keys()]
+        bases_no_inject_partner = self.bot.townhalls.filter(lambda h: h.tag not in self.queen_home.values())
 
         if len(self.queens) == 1:
             queen.is_creep = True
-        if len(self.queens) > 1:
+        if len(self.queens) > 1 and bases_no_inject_partner.amount >= 1:
             for queen in queens_no_inject_partner:
                 if not queen.is_creep: 
-                    if bases_no_inject_partner.amount == 0:
-                        break
-                    else:
-                        closest_base = bases_no_inject_partner.closest_to(queen.position)
-                        queen.hatch_home[queen.tag] = closest_base.tag
-                        bases_no_inject_partner = bases_no_inject_partner - [closest_base]
-                        queen.is_hatch = True
-                        break
+                    closest_base = bases_no_inject_partner.closest_to(queen.position)
+                    self.queen_home[queen.tag] = closest_base.tag #dict of queens and their hatches
+                    queen.hatch_home[queen.tag] = closest_base.tag #Queen object attribute
+                    bases_no_inject_partner = bases_no_inject_partner - [closest_base]
+                    print(queen.hatch_home)
+                    print(self.queen_home)
+                    queen.is_hatch = True
+                    break
+        else:
+            queen.is_creep = True
 
 
     # else one hatch gets assigned twice
