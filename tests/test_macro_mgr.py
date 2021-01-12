401d81956e52120aa9b6a1ce35b79410f23e0287
wj <whompyjaw@pm.me>
2021-01-11 22:30:09 -0800

    adding pytest

---
+++ tests/test_macro_mgr.py
@@ -0,0 +1,18 @@
+import pytest
+import sc2
+from eg
+from sc2 import BotAI
+from sc2.units import Units
+from sc2.unit import Unit
+from src.queen import Queen
+from sc2.ids.unit_typeid import UnitTypeId
+from src.managers.macro import MacroManager
+
+class TestUnitManager(unittest.TestCase):
+    def setUp(self):
+        self.bot = BotAI()
+        self.um = UnitManager()
+
+    def test_expand(self):
+        
+        pass
