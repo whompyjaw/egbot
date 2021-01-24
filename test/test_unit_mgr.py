1e625c050ac7dffaa7f120e4a10efc7ecb91e2ec
wj <whompyjaw@pm.me>
2021-01-24 13:17:54 -0800

    lots to do for testing.. need to figure out how to mock exceptions to silence

--- test/test_unit_mgr.py
+++ test/test_unit_mgr.py
@@ -1,17 +1,24 @@
-import pytest
-import sc2
-from sc2 import BotAI
-from sc2.units import Units
+from managers.unit_mgr import UnitManager
+from collections import defaultdict
+from units import Drone
 
-bot = EGBot()
+# bot = EGBot()
 
-@pytest.fixture
-def unit_manager():
-    return UnitManager()
+# @pytest.fixture
+# def unit_manager():
+#     return UnitManager()
 
-@pytest.fixture
-def units_dict():
-    um = unit_manager()
-    units = NestedDefaultDict()
-    units['Drone'][123] = Drone()
-    units['Drone'][1234] = Drone()
+# @pytest.fixture
+# def units_dict():
+#     um = unit_manager()
+#     units = NestedDefaultDict()
+#     units['Drone'][123] = Drone()
+#     units['Drone'][1234] = Drone()
+
+def test_add_unit():
+    bot = sc2.BotAI()
+    new_unit = Unit(UnitTypeID.DRONE, bot)
+    unit_tag = new_unit.tag
+    um = UnitManager(bot)
+    um.add_unit(new_unit)
+    assert um.units['Drone'].get(unit_tag)
