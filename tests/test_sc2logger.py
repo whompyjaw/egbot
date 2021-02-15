59aad978686552bb4df48c424c4534796a154ff0
Glenn <whompyjaw@pm.me>
2021-02-15 12:22:13 -0800

    Updates queens-sc2
    git pulled raspers new changes, and going to game test how the creep spread works with his edit suggestions.

--- tests/test_sc2logger.py
+++ tests/test_sc2logger.py
@@ -14,14 +14,14 @@ class TestSc2Logger(unittest.TestCase):
         self.bot.game_data = mock.Mock()
 
 
-    def test_distribution_workers(self):
+    def test_log_distribution_workers(self):
         expect = defaultdict()
         expect[5] = {}
         expect[5]['min'] = 12
         expect[5]['gas1'] = 3
         expect[5]['gas2'] = 2
 
-        self.bot.townhalls = 
+        self.bot.townhalls = None
 
         res = self.logger.worker_dirstribution(self.bot)
         assert expect == res
