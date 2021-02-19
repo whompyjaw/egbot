64a96cf6703f58e068930aee343ca5f099d848af
Eric Hughes <hughes.eric10@gmail.com>
2021-02-18 23:24:50 -0500

    added in some expac logic

--- test/test_pathing.py
+++ test/test_pathing.py
@@ -31,12 +31,17 @@ class PathBot(sc2.BotAI):
           # self.md.plot_influenced_path(start=self.hq_pos, goal=self.enemy_hq, weight_array=self.grid_points)
           # self.md.show()
           expacs = self.expansion_locations_list
+          close_expac_list = sorted(self.get_distances(self.hq_pos, expacs), key=lambda x:x[0])
+          
+          '''
+          Idea: Get closest expacs to hq (sorts by distance) - we need to spread creep between them.  Then we can find the furthest 
+          expacs from our hq by simply returning the end of 'close_expac_list' i.e. close_expac_list[-5:].  This
+          is a way for us to get the paths we want, possibly not the best way.  Next thought is to assign a queen
+          a specific path via a specific queen policy.  
+          '''
 
-          close_expacs = [expac for expac in expacs if self.distance_math_hypot(expac, self.hq_pos) < 50.0]
-
-          enemy_expacs = [expac for expac in expacs if self.distance_math_hypot(expac, self.enemy_hq) < 50.0]
-
-          for loc in enemy_expacs:
+          for x in close_expac_list[:7]:
+               loc = x[1]
                if self.hq_pos == loc:
                     continue
                # path = self.md.pathfind(self.hq_pos, loc, self.grid_points)
@@ -51,7 +56,14 @@ class PathBot(sc2.BotAI):
 
                
           # self.client.debug_line_out(self.hq_pos3, self.enemy_hq_pos3, color=(255, 255, 255))
-          
+     def get_distances(self, point, iterable: list):
+          dist_list = []
+
+          for x in iterable:
+               dist = self.distance_math_hypot(x, point)
+               dist_list.append(tuple([dist, x]))
+
+          return dist_list
 
 run_game(
         maps.get("AbyssalReefLE"),
